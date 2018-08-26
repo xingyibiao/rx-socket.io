44b26fd3a7597d3382141561df1c94fde78b9171
xingyibiao <xingyibiao@xingyibiaodeMacBook-Pro.local>
2018-08-26 15:35:51 +0800

    修改构造函数传参

--- rx.socket.js
+++ rx.socket.js
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 import { fromEvent, Observable } from 'rxjs'
 
 export default class RxSocket {
-  constructor({url, option}) {
+  constructor(url, option) {
     this.socket = io.connect(url, option)
   }
 
