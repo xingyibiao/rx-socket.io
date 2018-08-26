1232f55b957ca47d14ed5e43bcbaaba54cd4e09b
xingyibiao <xingyibiao@xingyibiaodeMacBook-Pro.local>
2018-08-26 15:27:43 +0800

    first commit

---
+++ rx.socket.js
@@ -0,0 +1,20 @@
+import io from "socket.io-client";
+import { fromEvent, Observable } from 'rxjs'
+
+export default class RxSocket {
+  constructor({url, option}) {
+    this.socket = io.connect(url, option)
+  }
+
+  on(eventName) {
+    return fromEvent(this.socket, eventName)
+  }
+
+  emit(type, ...data) {
+    return Observable.create(obs => {
+      this.socket.emit(type, ...data, (e) => {
+        obs.next(e)
+      })
+    })
+  }
+}
